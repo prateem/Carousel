1edb04b1738f8520079455700123836b2109d660
Prateem Shrestha <prateems@gmail.com>
2019-09-30 08:54:31 -0400

    Remove default center gravity on viewholder container. Update example adapters to keep center gravity.

--- app/src/main/java/com/example/meetarp/carousel/CarouselImagesAdapter.kt
+++ app/src/main/java/com/example/meetarp/carousel/CarouselImagesAdapter.kt
@@ -1,22 +1,20 @@
 package com.example.meetarp.carousel
 
 import android.content.Context
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.RelativeLayout
-import com.meetarp.carousel.R
 import com.meetarp.carousel.CarouselAdapter
 import com.example.meetarp.carousel.data.CarouselImage
 
 class CarouselImagesAdapter : CarouselAdapter<CarouselImage>() {
 
-    fun getErrorView(context: Context): View? {
-        val iconSize = context.resources.getDimensionPixelSize(R.dimen.error_icon_size)
-
-        return ImageView(context).also {
-            it.layoutParams = RelativeLayout.LayoutParams(iconSize, iconSize)
-            it.setImageResource(R.drawable.image_error)
-        }
+    // Default gravity is START and TOP. This adapter wants the views CENTER in the container.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselViewHolder {
+        return super.onCreateViewHolder(parent, viewType)
+            .also { vh -> vh.container.gravity = Gravity.CENTER }
     }
 
     override fun bindItemForPosition(
@@ -35,6 +33,15 @@ class CarouselImagesAdapter : CarouselAdapter<CarouselImage>() {
             }
 
         item.loadInto(imageView, this, holder)
+    }
+
+    fun getErrorView(context: Context): View? {
+        val iconSize = context.resources.getDimensionPixelSize(R.dimen.error_icon_size)
+
+        return ImageView(context).also {
+            it.layoutParams = RelativeLayout.LayoutParams(iconSize, iconSize)
+            it.setImageResource(R.drawable.error)
+        }
     }
 
 }
--- app/src/main/java/com/example/meetarp/carousel/CarouselViewsAdapter.kt
+++ app/src/main/java/com/example/meetarp/carousel/CarouselViewsAdapter.kt
@@ -1,9 +1,17 @@
 package com.example.meetarp.carousel
 
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import com.meetarp.carousel.CarouselAdapter
 
 class CarouselViewsAdapter : CarouselAdapter<View>() {
+
+    // Default gravity is START and TOP. This adapter wants the views CENTER in the container.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarouselViewHolder {
+        return super.onCreateViewHolder(parent, viewType)
+            .also { vh -> vh.container.gravity = Gravity.CENTER }
+    }
 
     override fun bindItemForPosition(holder: CarouselViewHolder, position: Int, item: View) {
         holder.progressBar.visibility = View.GONE
